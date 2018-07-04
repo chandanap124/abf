7b2c8697caeda16a470da10995b4f90c9aaa73b0
Chandanap124 <chandanap124@gmail.com>
2018-07-04 11:05:38 +0530

    Boostrap SS JS Updated

---
+++ src/components/Content.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+
+export default class Content extends Component {
+    render(){
+        return (
+            <div className="sufee-login d-flex align-content-center flex-wrap">
+                <div className="container">
+                    <div className="login-content">
+                        <div className="login-logo">
+                            <a href="index.html">
+                                <img className="align-content" src="images/logo.png" alt=""></img>
+                            </a>
+                        </div>
+
+                    </div>
+                </div>
+
+
+
+
+
+
+            </div>
+
+
+
+
+
+
+
+        )
+    }
+}
